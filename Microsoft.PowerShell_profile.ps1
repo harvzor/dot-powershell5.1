1d501c3eb0ede3ad4bfdd4b437841326a375eafd
harvzor <harveyjwilliams@aol.co.uk>
2026-04-22 08:45:58 +0200

    Disable update messages.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,5 @@
-function Add-Timestamp {
-    process {
-        Write-Output "$([string]::Format('{0:HH:mm:ss.fff} -- {1}', (Get-Date), $_))"
-    }
-}
+# Stop Powershell release messages
+[System.Environment]::SetEnvironmentVariable('POWERSHELL_UPDATECHECK', 'Off', 'User')
 
 # https://github.com/ajeetdsouza/zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
@@ -14,7 +11,6 @@ Set-Alias -Name ls -Value eza-icons -Option AllScope -Force
 
 Set-Alias -Name vim -Value nvim -Force
 
-
 # OPENSPEC:START - OpenSpec completion (managed block, do not edit manually)
-. "C:\Users\HarveyWilliams\Documents\PowerShell\OpenSpecCompletion.ps1"
+. "C:\Users\Harve\Documents\PowerShell\OpenSpecCompletion.ps1"
 # OPENSPEC:END
